ec99eb06bb0f0b06f8f7b57f1d1751ebc4553567
Sephanne Flor <sephanneflor9@gmail.com>
2025-10-03 00:07:24 -0700

    Create Big Sorting.cs

---
+++ Big Sorting.cs
@@ -0,0 +1,58 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'bigSorting' function below.
+     *
+     * The function is expected to return a STRING_ARRAY.
+     * The function accepts STRING_ARRAY unsorted as parameter.
+     */
+
+    public static List<string> bigSorting(List<string> unsorted)
+    {
+         // Sort first by length, then lexicographically for strings of the same length
+        return unsorted
+                .OrderBy(s => s.Length)
+                .ThenBy(s => s, StringComparer.Ordinal)
+                .ToList();
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<string> unsorted = new List<string>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string unsortedItem = Console.ReadLine();
+            unsorted.Add(unsortedItem);
+        }
+
+        List<string> result = Result.bigSorting(unsorted);
+
+        textWriter.WriteLine(String.Join("\n", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
